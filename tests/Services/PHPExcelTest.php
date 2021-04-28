1522db72f19081cb56616c080ce910093b75ac14
TomasVotruba <tomas.vot@gmail.com>
2021-04-28 16:18:34 +0200

    fixup! remove uneeded excel and classmap autoload

--- tests/Services/PHPExcelTest.php
+++ tests/Services/PHPExcelTest.php
@@ -10,6 +10,30 @@ final class PHPExcelTest extends TestCase
     public function testCanBeCreated()
     {
         $objPHPExcel = new PHPExcel();
-        $objPHPExcel->getProperties()->setCreator("Maarten Balliauw");
+        $objPHPExcel->getProperties()->setCreator("Maarten Balliauw")
+            ->setLastModifiedBy("Maarten Balliauw")
+            ->setTitle("Office 2007 XLSX Test Document")
+            ->setSubject("Office 2007 XLSX Test Document")
+            ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+            ->setKeywords("office 2007 openxml php")
+            ->setCategory("Test result file");
+
+        $objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue('A1', 'Hello')
+            ->setCellValue('B2', 'world!')
+            ->setCellValue('C1', 'Hello')
+            ->setCellValue('D2', 'world!');
+
+        $objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue('A4', 'Miscellaneous glyphs')
+            ->setCellValue('A5', 'éàèùâêîôûëïüÿäöüç');
+        $objPHPExcel->getActiveSheet()->setTitle('Simple');
+        $objPHPExcel->setActiveSheetIndex(0);
+        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+        $file = '/tmp/test.xlsx';
+        if(file_exists($file))
+            unlink($file);
+        $objWriter->save($file);
+        $this->assertFileExists($file);
     }
 }
