5cf295d0187f2ac0fb406b6bbfa6f652c401ca42
TomasVotruba <tomas.vot@gmail.com>
2021-04-28 11:53:50 +0200

    narrow rules to broken pair

--- rector.php
+++ rector.php
@@ -3,11 +3,23 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
+use Rector\PHPOffice\Rector\StaticCall\AddRemovedDefaultValuesRector;
+use Rector\Renaming\Rector\Name\RenameClassRector;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [__DIR__ . '/tests']);
-    $parameters->set(Option::SETS, [SetList::PHPEXCEL_TO_PHPSPREADSHEET]);
+    $services = $containerConfigurator->services();
+
+    // this rule is causing troubles
+    $services->set(AddRemovedDefaultValuesRector::class);
+
+    $services->set(RenameClassRector::class)
+        ->call('configure', [[
+            RenameClassRector::OLD_TO_NEW_CLASSES => [
+                'PHPExcel' => 'PhpOffice\PhpSpreadsheet\Spreadsheet',
+            ]
+        ]]);
 };
