9636a9b8e1b2a10fc5083dc0fa655dbc45862318
TomasVotruba <tomas.vot@gmail.com>
2021-04-28 11:49:16 +0200

    restore test fixture

--- tests/Services/PHPExcelTest.php
+++ tests/Services/PHPExcelTest.php
@@ -10,6 +10,31 @@ final class PHPExcelTest extends TestCase
     //Création d'un fichier Excel ?
     public function testCanBeCreated()
     {
-        $objPHPExcel = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+        $objPHPExcel = new PHPExcel();
+        $objPHPExcel->getProperties()->setCreator("Maarten Balliauw")
+            ->setLastModifiedBy("Maarten Balliauw")
+            ->setTitle("Office 2007 XLSX Test Document")
+            ->setSubject("Office 2007 XLSX Test Document")
+            ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+            ->setKeywords("office 2007 openxml php")
+            ->setCategory("Test result file");
+
+        $objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue('A1', 'Hello')
+            ->setCellValue('B2', 'world!')
+            ->setCellValue('C1', 'Hello')
+            ->setCellValue('D2', 'world!');
+
+        $objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue('A4', 'Miscellaneous glyphs')
+            ->setCellValue('A5', 'éàèùâêîôûëïüÿäöüç');
+        $objPHPExcel->getActiveSheet()->setTitle('Simple');
+        $objPHPExcel->setActiveSheetIndex(0);
+        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+        $file = '/tmp/test.xlsx';
+        if(file_exists($file))
+            unlink($file);
+        $objWriter->save($file);
+        $this->assertFileExists($file);
     }
 }
